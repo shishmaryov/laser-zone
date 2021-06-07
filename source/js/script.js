5e8e833b0677a477f9ae644a2abc821fed3bc8b3
Konstantin <shishmaryovkonstantin@gmail.com>
2021-06-07 11:54:56 +0300

    scroll

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,53 @@
+/*
+$(document).ready(function(){
+  $('.nav__link').click(function(e){
+      if($(this).attr('href').indexOf('#') != -1){
+      e.preventDefault();
+            var href = $(this).attr('href').replace('#', '');
+      if($('#'+href).length > 0){
+              var tophref = $('body').find('#'+href).offset().top;
+              $('html, body').animate({scrollTop: tophref}, 800);
+      }
+    }
+    });
+});
+*/
+
+// собираем все якоря; устанавливаем время анимации и количество кадров
+var anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
+      animationTime = 500,
+      framesCount = 60;
+
+anchors.forEach(function(item) {
+  // каждому якорю присваиваем обработчик события
+  item.addEventListener('click', function(e) {
+    // убираем стандартное поведение
+    e.preventDefault();
+
+    // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
+    var coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
+
+    // запускаем интервал, в котором
+    var scroller = setInterval(function() {
+      // считаем на сколько скроллить за 1 такт
+      var scrollBy = coordY / framesCount;
+
+      // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
+      // и дно страницы не достигнуто
+      if(scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+        // то скроллим на к-во пикселей, которое соответствует одному такту
+        window.scrollBy(0, scrollBy);
+      } else {
+        // иначе добираемся до элемента и выходим из интервала
+        window.scrollTo(0, coordY);
+        clearInterval(scroller);
+      }
+    // время интервала равняется частному от времени анимации и к-ва кадров
+    }, animationTime / framesCount);
+  });
+});
+
+
 new Swiper('.reviews__slider', {
   pagination: {
     el: '.swiper-pagination',
