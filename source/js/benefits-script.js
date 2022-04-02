8a0b36f178c1da4d86c9b50a48bf8d2bebce9aa7
Konstantin <shishmaryovkonstantin@gmail.com>
2022-04-02 19:53:53 +0300

    news page mobile version

--- source/js/benefits-script.js
+++ source/js/benefits-script.js
@@ -3,22 +3,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	animationTime = 500,
 	framesCount = 60;
 
-anchors.forEach(function (item) {
-	item.addEventListener('click', function (e) {
-		e.preventDefault();
-		var coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
-		var scroller = setInterval(function () {
-			var scrollBy = coordY / framesCount;
-			if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
-				window.scrollBy(0, scrollBy);
-			} else {
-				window.scrollTo(0, coordY);
-				clearInterval(scroller);
-			}
-		}, animationTime / framesCount);
-	});
-});
-
 var headerMobile = document.querySelector('.header__mobile');
 var burger = document.querySelector('.header__burger');
 var headerClose = document.querySelector('.header__close');
